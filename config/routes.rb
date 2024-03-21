c872f1b75e153feb24d46c6b9ee5fd6b450984c1
UshaSuwal <usaasuwa@gmail.com>
2024-03-21 12:08:36 +0545

    Create post for instapost

---
+++ config/routes.rb
@@ -0,0 +1,12 @@
+Rails.application.routes.draw do
+  # root 'home#index'
+
+  resources :users
+  resources :sessions, only: [:new, :create, :destroy]
+  get 'signup', to: 'users#new', as: 'signup'
+  get 'login', to: 'sessions#new', as: 'login'
+  delete 'logout', to: 'sessions#destroy', as: 'logout'
+
+  root 'posts#index'
+  resources :posts
+end
