f0b64bbe5e0a8461c73675c9a0aecf5e897a5234
UshaSuwal <usaasuwa@gmail.com>
2024-03-22 13:51:53 +0545

    added delete

--- app/models/post.rb
+++ app/models/post.rb
@@ -2,6 +2,7 @@ class Post < ApplicationRecord
   belongs_to :user
 
   has_one_attached :photo
+  has_many :comments
 
   validates :photo, :description, :user_id, presence: true
 end
--- config/routes.rb
+++ config/routes.rb
@@ -9,4 +9,8 @@ Rails.application.routes.draw do
 
   root 'posts#index'
   resources :posts
+
+  resources :posts do
+    resources :comments
+  end
 end
